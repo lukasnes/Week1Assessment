7aed0e285e0ad1ecfcc72b8f0711c26986aca6a8
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-07 09:35:13 -0600

    FirstPartComplete

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, after having determined that the code needs to be addended or changed, developers need to figure out a baseline for their goals in this project."
+const design = "This next step of the process is where the pens hit the paper, and the developers begin to hash out the code"
+const implementation = "Once the new code has been designed, it's time to incorporate it into the project."
+const testingAndIntegration = "Now that we have the new code layered into our project, it's time to test for bugs, and fully integrate the new design"
+const maintain = "Code is rarely completely bug free, and users will sniff out those bugs, no matter how much the developers tested. After the new code has been integrated, the systems need to be maintained, and bugs need to be caught and exterminated."
