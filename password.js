9d9d9755fa70b2c848d83e9698385803ca959325
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-07 11:48:15 -0600

    Password successfully validated

--- password.js
+++ password.js
@@ -1,4 +1,5 @@
 console.log("Welcome to the password validator!")
+console.log("Your password must be 10 characters long and include at least one uppercase letter, one lowercase letter, and a number.")
 const { read } = require('fs');
 const readline = require('readline');
 
@@ -24,8 +25,11 @@ reader.question("Please validate your password. ", function(input){
     if(input.length >= 10 && hasLowerCase(input) && hasUpperCase(input) && containsNumber(input)){
         console.log("Password successfully validated!")
     } else {
-        console.log("Password validation was unsuccessful")
+        console.log("Password validation was unsuccessful.")
     }
 
     reader.close()
 });
+
+
+//Note, I attempted to put ASCII into the messages for over half an hour, and couldn't get it to work. I prefer my code to be clean and workable, so I deleted any remnants of it.
