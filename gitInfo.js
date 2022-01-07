a0039025cb1557d7216b27c0758427bbbc012509
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-07 10:07:35 -0600

    GitInfoComplete

--- gitInfo.js
+++ gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = "Git is a local cloud for coding projects, to which you can save instances of your code to pull from in case you encounter an error in your code that is unsalvagable. It creates version histories for your code with whatever message you want to remind yourself which version is which."
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = "GitHub is a public platform which, much like git, acts like a cloud for coding projects. The difference between Github and Git being that GitHub not only allows you to save your projects to the domain, but other people can also view, and even edit your files. It is an extremely useful tool whenever you are working with a team on a project."
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+let gitInitDefinition = "The git init command initializes whichever directory you use it on into the git cloud, creating a space in that directory to save your data to."
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+var gitCloneDefinition = "The git clone command allows you to create a copy of an existing repository in a new directory, in a different place in your files."
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+let gitStatusDefinition = "The git status command allows you to see what in your current directory has been uploaded into git, what changes haven't been saved yet, and which files are not being tracked by git currently."
 //////////////////PROBLEM 6////////////////////
 /*
     Create a variable called 'gitAddDefinition'.  
@@ -53,7 +53,8 @@
 */
 
 //CODE HERE
-
+let gitAddDefinition = "The git add command adds files in your git initialized directory into the git cloud."
+let gitAddCode = "git add ."
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,7 +65,8 @@
 */
 
 //CODE HERE
-
+let gitCommitDefinition = "The git commit command creates a snapshot of the files you've added to git. This snapshot is something that you can return to in case your code gets unmanageable and you have to return to a previous version."
+let gitCommitCode = "git commit -m 'initial commit'"
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +75,4 @@
 */
 
 //CODE HERE
+let gitPushDefinition = "The git push command is what allows us to send our code to external repositories like GitHub."
