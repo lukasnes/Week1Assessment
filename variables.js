031f6be7fa5636402bd36bcc87bad016c0d666fd
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-07 09:39:24 -0600

    VariablesDone

--- variables.js
+++ variables.js
@@ -2,17 +2,17 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+let myName = "Lukas"
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+let faveNum = 5
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+let lovesCode = true
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,10 +21,17 @@
 */
 
 //CODE HERE
-
+if(faveNum === 13){
+  console.log('Lucky 13!')
+} else {
+  console.log('Not lucky 13.')
+}
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+for(i = 0; i < 5; i++){
+  console.log(faveNum)
+}
