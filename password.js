dcb7bdc2aa2a0164f7a389d95646e688d4b2cec3
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-01-07 11:15:11 -0600

    ComplicatedPassword

--- password.js
+++ password.js
@@ -6,12 +6,26 @@ const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+function hasLowerCase(input) {
+    return input.toUpperCase() != input;
+}
+function hasUpperCase(input) {
+    return input.toLowerCase() != input;
+}
+function containsNumber(input) {
+    for(i = 0; i < input.length; i++){
+        if(isNaN(input[i]) === false){
+            return input;
+        }
+    }
+}
 
 reader.question("Please validate your password. ", function(input){
-    if(input.length >= 10){
+    if(input.length >= 10 && hasLowerCase(input) && hasUpperCase(input) && containsNumber(input)){
         console.log("Password successfully validated!")
     } else {
         console.log("Password validation was unsuccessful")
     }
+
     reader.close()
 });
